2a93c6a1f5aa460fe34cffe81126b7fbeb9bcb3f
Omar A <114465136+omaroaalj@users.noreply.github.com>
2023-04-03 11:50:55 -0600

    Displayed username on screen

---
+++ Coin Rush CMPT 322/Assets/UsernameText.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UsernameText : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI username;
+
+    public void setUsername(string newUsername) {
+        username.text = newUsername;
+    }
+
+    public string getUsername() {
+        return username.text;
+    }
+
+    void Start() {
+        setUsername(PlayerPrefs.GetString("user_name", "NAME NOT FOUND"));
+    }
+}
