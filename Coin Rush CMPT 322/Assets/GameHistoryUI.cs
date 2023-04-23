ec9fd02d35f01b3d93ac8e6dbd58262282ad18e4
Omar A <114465136+omaroaalj@users.noreply.github.com>
2023-04-23 17:44:05 -0600

    Show game history on menu click

--- Coin Rush CMPT 322/Assets/GameHistoryUI.cs
+++ Coin Rush CMPT 322/Assets/GameHistoryUI.cs
@@ -1,18 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameHistoryUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public static void createMathLogButton(TextMeshProUGUI buttonToCopy) {
+        Instantiate(buttonToCopy);
     }
 }
