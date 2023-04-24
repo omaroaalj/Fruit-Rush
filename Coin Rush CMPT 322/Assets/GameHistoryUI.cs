aaef7e9330fcfc23a09dd441e958fbfb60fce9b3
Omar A <114465136+omaroaalj@users.noreply.github.com>
2023-04-23 18:41:35 -0600

    Repeat buttons

--- Coin Rush CMPT 322/Assets/GameHistoryUI.cs
+++ Coin Rush CMPT 322/Assets/GameHistoryUI.cs
@@ -5,7 +5,24 @@ using TMPro;
 
 public class GameHistoryUI : MonoBehaviour
 {
-    public static void createMathLogButton(TextMeshProUGUI buttonToCopy) {
-        Instantiate(buttonToCopy);
+    private static int moveDownValue = 0;
+
+    private static GameObject container;
+    private static GameObject buttonTemplate;
+
+    public static void setContainer(GameObject currentContainer) {
+        container = currentContainer;
     }
+
+    public static void setMathLogButton(GameObject buttonToCopy) {
+        buttonTemplate = buttonToCopy;
+    }
+
+    public static void createLogButtons() {
+        for (int i = 0; i < 5; i++) {
+            Instantiate(buttonTemplate,container.transform).transform.Translate(new Vector3(0,moveDownValue,0));
+            moveDownValue -= 100;
+        }
+    }
+
 }
