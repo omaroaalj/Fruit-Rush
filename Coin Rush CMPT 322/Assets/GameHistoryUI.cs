5c5ccdc1d4cd0abe36dc2db2480510a2250eabf2
Omar A <114465136+omaroaalj@users.noreply.github.com>
2023-04-24 17:50:32 -0600

    Update GameHistoryUI.cs

--- Coin Rush CMPT 322/Assets/GameHistoryUI.cs
+++ Coin Rush CMPT 322/Assets/GameHistoryUI.cs
@@ -6,53 +6,27 @@ using TMPro;
 
 public class GameHistoryUI : MonoBehaviour
 {
-
     [SerializeField] private TextMeshProUGUI logText;
 
     public void setLogText() {
         string mathLog = "";
         string filePath = @"MathLog.txt";
-        var logFile = File.ReadAllLines(filePath);
-        foreach (string line in logFile) {
-            Debug.Log("Line: " + line);
-            mathLog += (line + "\n");
-            //int lastUsernameChar = logFile.Length-18;
-            //StreamReader streamText = new StreamReader(logFile);
-            //Debug.Log("File reading complete!");
-            //string line;
-            //while ((line = streamText.ReadLine()) != null) {
-            //    mathLog += (line + "\n");
-            //}
-            //mathLog += "-----\n";
+        try {
+            var logFile = File.ReadAllLines(filePath);
+            foreach (string line in logFile) {
+                Debug.Log("Line: " + line);
+                mathLog += (line + "\n");
+            }
+            Debug.Log("Math Log read complete.");
+            logText.text = mathLog;
+        } catch (FileNotFoundException e) {
+            Debug.Log(e.Message);
         }
-        Debug.Log("Math Log read complete.");
-        logText.text = mathLog;
+        //foreach (string line in logFile) {
+        //    Debug.Log("Line: " + line);
+        //    mathLog += (line + "\n");
+        //}
+        //Debug.Log("Math Log read complete.");
+        //logText.text = mathLog;
     }
-
-    //public static void collectLogs() {
-    //    string folderPath = @"MathLog";
-    //    var logFiles = Directory.EnumerateFiles(folderPath);
-    //}
-
-    //private static int moveDownValue = 0;
-
-    //private static GameObject container;
-    //private static GameObject buttonTemplate;
-
-    //public static void setContainer(GameObject currentContainer) {
-    //    container = currentContainer;
-    //}
-
-    //public static void setMathLogButton(GameObject buttonToCopy) {
-    //    buttonTemplate = buttonToCopy;
-    //}
-
-    //public static void createLogButtons() {
-    //    for (int i = 0; i < 5; i++) {
-    //        GameObject copyButton = Instantiate(buttonTemplate,container.transform);
-    //        copyButton.transform.Translate(new Vector3(0,moveDownValue,0));
-    //        moveDownValue -= 50;
-    //    }
-    //}
-
 }
